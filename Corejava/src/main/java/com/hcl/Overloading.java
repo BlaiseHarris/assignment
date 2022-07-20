0cf05bdc902e5d9b2db8db63b3eb10fee85fa77c
BlaiseHarris <94414385+BlaiseHarris@users.noreply.github.com>
2022-07-20 12:01:09 -0400

    Add files via upload
    This is from my daily classes in the Saran batch.

---
+++ Corejava/src/main/java/com/hcl/Overloading.java
@@ -0,0 +1,19 @@
+package com.hcl;
+
+import java.io.PrintStream;
+
+public class Overloading {
+	 public static int sum(int a, int b) {
+		 return a + b;
+	 }
+	 static int sum(int a, int b, int c) {
+		 return a + b + c;
+	 }
+	 
+	 public static void main(String[] args) {
+		int x = sum(1, 2);
+		System.out.println(x);
+		int z = sum(1, 2, 3);
+		System.out.println(z);
+	}
+}
