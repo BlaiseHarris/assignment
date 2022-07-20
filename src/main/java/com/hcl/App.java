2cb410f779f4fa46d659a3401e017bad2f23c505
BlaiseHarris <baharris5@my.waketech.edu>
2022-07-19 20:05:35 -0400

    These are only additions on the test branch

--- src/main/java/com/hcl/App.java
+++ src/main/java/com/hcl/App.java
@@ -8,6 +8,9 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!");
+    	int x = 0;
+    	System.out.println("The change: " + x);
+    	System.out.println( "Hello World!");
+        
     }
 }
