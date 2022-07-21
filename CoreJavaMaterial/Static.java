3a3b74c4ba512f1daf0614a0d2b3f53eba612b08
BlaiseHarris <baharris5@my.waketech.edu>
2022-07-21 17:35:53 -0400

    Catching up on meeting

---
+++ CoreJavaMaterial/Static.java
@@ -0,0 +1,15 @@
+package com.hcl;
+
+public class Static {
+
+	static int y = 0;
+	Static(){
+		y++; // 0
+		System.out.println(y); // 1
+	}
+	public static void main(String[] args) {
+		Static s1 = new Static(); // 1 w/o static  1 w/ static before variable y
+		Static s2 = new Static(); // 1             2
+		Static s3 = new Static(); // 1             3
+	}
+}
