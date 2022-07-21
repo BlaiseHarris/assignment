cd46121c388717951af1e6a1f6feac356aa2c6ce
BlaiseHarris <baharris5@my.waketech.edu>
2022-07-21 17:37:11 -0400

    finalizing some of my changes

--- src/main/java/com/hcl/App.java
+++ src/main/java/com/hcl/App.java
@@ -6,8 +6,12 @@ package com.hcl;
  */
 public class App 
 {
+	
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!");
+    	for(int i=0; i<10; i++) {
+    		System.out.println("Welcome: " + i);
+    	}
+        
     }
 }
